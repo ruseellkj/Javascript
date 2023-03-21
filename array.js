198069496a76032fcfe1ea40f5363ac9099a14ad
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-21 17:06:44 +0530

    arr

---
+++ array.js
@@ -0,0 +1,13 @@
+const new_array = [1,2,3,4,5,6,7,8,9]
+let i =0
+for(i =0; i<new_array.length; i++){
+    console.log(new_array[i])
+}
+
+// adding a new element to the array
+new_array[9] = 10
+// changing an element of the Array
+new_array[0] = 89
+
+console.log(new_array)
+console.log(typeof new_array)
