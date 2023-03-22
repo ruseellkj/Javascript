082570abf5ecfe266374bd2feca14a8e7445ecd1
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-22 16:32:15 +0530

    console oj methods

---
+++ js_console_object.js
@@ -0,0 +1,7 @@
+// console is an obeject and it has various methods 
+// like log , table, clea etc
+let obj = {a: 1, b: 2, c:3}
+console.table(obj)
+console.warn("hey dont click here ")
+console.time("a")
+console.timeEnd("a")
