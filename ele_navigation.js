770693bec6fc890f0395b0bad3da2505ed61efea
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-22 19:46:55 +0530

    update

--- ele_navigation.js
+++ ele_navigation.js
@@ -1,3 +1,10 @@
+const chgeBG = () =>{
+    document.body.firstElementChild.style.background = "red"
+}
 let b = document.body
-console.log(b.firstChild) // #text
-console.log(b.firstElementChild) // element that is nav
+console.log("first child of b is" ,b.firstChild) // #text
+console.log("first elemen child of b is", b.firstElementChild) // element that is nav
+
+
+document.children
+// childElementNodes = children
