e8cc7cc3feaa35416238a85c38d7740975cae99e
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-22 23:47:10 +0530

    modified and corrected the prgm

---
+++ prac_dom.js
@@ -0,0 +1,7 @@
+// problem 1 
+document.getElementsByTagName("nav")[0].firstElementchild.style.color = "red"
+
+Array.from(document.getElementsByTagName("li")).forEach((element) => {
+    element.style.background = "cyan"
+})
+
