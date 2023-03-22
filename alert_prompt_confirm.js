0886969d2d2c63261c3b2f0ca7d0d4e4943f14f1
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-22 16:52:42 +0530

    alert,prompt,confirm

---
+++ alert_prompt_confirm.js
@@ -0,0 +1,12 @@
+alert("enter the value of a ")
+let a = prompt("enter a here ")
+a = Number.parseInt(a)
+alert("the entered thing is of type" + (typeof(a)))
+let write =confirm("do u really want to write?")
+if(write)
+{
+    document.write(a)
+}
+else{
+    document.write("please allow me to write here ")
+}
