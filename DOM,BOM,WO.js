4c8590d8670b11e2229fe877e30ba981ec15adfd
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-22 17:06:52 +0530

    dom,bom,wo

---
+++ DOM,BOM,WO.js
@@ -0,0 +1,8 @@
+// DOM is document object model that means that pura html ka ek object ban gya hai and uska naam is document 
+// window object is a global object
+console.log(window)
+console.log(document.body)
+document.body.style.background = "red"
+
+// BOM : additional object that host environment provides 
+// alert , prompt and confirm are BOM
