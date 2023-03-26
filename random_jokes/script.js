28dc66ecb48ec3b8d56cc0967a4deed04f61481d
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-26 17:02:00 +0530

    minute changes

--- random_jokes/script.js
+++ random_jokes/script.js
@@ -68,11 +68,11 @@ let jokes = ["Why don't grasshoppers watch football? They prefer cricket!",
 
 
   const myfunction = () =>{
-    let output = document.getElementById("output")
+    let out = document.getElementById("output")
     // this line generates random jokes out of 35 jokes array
     let rand_jokes = Math.ceil(Math.random() * 35)
     console.log(jokes[rand_jokes])
-    output.innerHTML = jokes[rand_jokes]
+    out.innerHTML = jokes[rand_jokes]
 
 
   }
--- sn,wt,gn.js
+++ sn,wt,gn.js
@@ -51,4 +51,3 @@ while (playagain) {
   }
   playagain = confirm("do u want to play again and win the game ?")
 }
-
