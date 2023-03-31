b5c5600c8fd4e9e15cb672c46dfcc808b8528e8b
Rushil Jariwala <rkjjariwala@gmail.com>
2023-03-31 16:56:42 +0530

    multiple handlers

---
+++ promises_multiple_handlers.js
@@ -0,0 +1,24 @@
+let p1 = new Promise((resolve, reject) => {
+    // alert("Hey I am not resolved")
+    setTimeout(() => {
+            resolve(1);
+    }, 2000)
+})
+
+p1.then(() => {
+    console.log("Hurray")
+    return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                    resolve(4)
+            }, 6000)
+    })
+}).then((value) => { 
+    console.log(value) 
+})
+
+p1.then(() => {
+    console.log("Congratulations this promise is now resolved")
+})
+p1.then(() => {
+    console.log("yes im rushil")
+})
