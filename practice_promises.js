648efed26ed819ebda30443b16ea09965f319ef4
Rushil Jariwala <rkjjariwala@gmail.com>
2023-04-02 10:55:02 +0530

    problem 3

--- practice_promises.js
+++ practice_promises.js
@@ -1,6 +1,6 @@
 // implement promises in the loadscript
 
-// q1
+// problem number 1 and 2
 const loadscript = async (src) => {
     // creating a new promise 
     return new Promise((resolve, reject) => {
@@ -17,15 +17,35 @@ const loadscript = async (src) => {
 
 }
 // alternate method to do
-let a = await loadscript("https://cdn.javascript")
+let a = loadscript("https://cdn.javascript")
 a.then((value) =>{
     console.log(value)
 })
-
+ 
 // const main1 = async () =>{
 //     console.log(new Date().getMilliseconds())
 //     let a = await loadscript("https://cdn.javascript/rushil")
 //     console.log(a)
 //     console.log(new Date().getMilliseconds())
 // }
-main1()
+// main1()
+
+// problem number 3
+let p = () =>{
+    new Promise((resolve,reject) => {
+    setTimeout(() => {
+        reject(new Error("this will not work as it is rejected"))
+    }, 3000)
+})
+}
+let a1 = async () =>{
+    try{
+        let c = await p()
+    console.log(c)
+    }catch(error){
+        console.log(error)
+        or
+        console.log("this error has been handled")
+    }
+}
+a1()
