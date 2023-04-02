446c48dc22144b31f78df68336f6b37a7b162d70
Rushil Jariwala <rkjjariwala@gmail.com>
2023-04-02 11:30:29 +0530

    solved qs

--- practice_promises.js
+++ practice_promises.js
@@ -18,10 +18,10 @@ const loadscript = async (src) => {
 }
 // alternate method to do
 let a = loadscript("https://cdn.javascript")
-a.then((value) =>{
+a.then((value) => {
     console.log(value)
 })
- 
+
 // const main1 = async () =>{
 //     console.log(new Date().getMilliseconds())
 //     let a = await loadscript("https://cdn.javascript/rushil")
@@ -31,21 +31,65 @@ a.then((value) =>{
 // main1()
 
 // problem number 3
-let p = () =>{
-    new Promise((resolve,reject) => {
-    setTimeout(() => {
-        reject(new Error("this will not work as it is rejected"))
-    }, 3000)
-})
+let p = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error("this will not work as it is rejected"))
+        }, 3000)
+    })
 }
-let a1 = async () =>{
-    try{
+
+let a1 = async () => {
+    try {
         let c = await p()
-    console.log(c)
-    }catch(error){
+        console.log(c)
+    } catch (error) {
         console.log(error)
         or
         console.log("this error has been handled")
     }
 }
 a1()
+
+// problem 4
+let p1 = async () =>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            resolve(41)
+        }, 2000);
+    })
+}
+
+let p2 = async () =>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            resolve(10)
+        }, 1000);
+    })
+}
+
+let p3 = async () =>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            resolve(20)
+        }, 3000);
+    })
+}
+
+const run = async () =>{
+    console.time("run")
+    // let a1 = await p1()
+    // let a2 = await p2()
+    // let a3 = await p3()
+
+    let a1 = p1()
+    let a2 = p2()
+    let a3 = p3()
+
+    let a1a2a3 = await Promise.all([a1,a2,a3])
+    console.log(a1a2a3)
+    console.log(a1,a2,a3)
+    console.timeEnd("run")
+}
+
+run()
